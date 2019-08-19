472565cfeca7ed438cc17f196952e6ac248ee693
Giovanne <giovanneafonso@gmail.com>
2019-08-18 21:31:34 -0500

    WIP - autocomplete with package.json scripts

--- packag.js
+++ packag.js
@@ -1,1 +1,15 @@
-console.log('test');
+#!/usr/bin/env node
+
+import * as omelette from 'omelette';
+import packageJson from './package.json';
+
+const scriptArgument = ({ reply }) => {
+  let scripts = Object.keys(packageJson.scripts)
+  reply(scripts)
+}
+
+function Packag() {
+  omelette`packag ${scriptArgument}`.init()
+}
+
+module.exports = Packag
